4c3e251cc2c24c590cc4d666a09a953e85212647
mattbulley <mattbulley01@gmail.com>
2023-10-10 02:50:37 +0100

    Added ImGui events, removed ImGui submodule and replaced with v1.68.

--- MAAT/src/MAAT/ImGui/ImGuiLayer.h
+++ MAAT/src/MAAT/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "MAAT/Layer.h"
 
+#include "MAAT/Events/ApplicationEvent.h"
+#include "MAAT/Events/KeyEvent.h"
+#include "MAAT/Events/MouseEvent.h"
+
 namespace MAAT {
 
 	class MAAT_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace MAAT {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
