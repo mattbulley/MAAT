eef4cf49ae76c42f4d0f81af7f86c9e28d93ab08
mattbulley <mattbulley01@gmail.com>
2023-10-13 00:08:01 +0100

    Added Transforms.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public MAAT::Layer
 {
 public:
@@ -58,6 +60,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -66,7 +69,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -93,13 +96,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -121,6 +125,7 @@ public:
 
 	void OnUpdate(MAAT::Timestep ts) override
 	{
+		//----- Left, Right, Up, Down ----------------------
 		if (MAAT::Input::IsKeyPressed(MAAT_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
 		else if (MAAT::Input::IsKeyPressed(MAAT_KEY_RIGHT))
@@ -131,6 +136,7 @@ public:
 		else if (MAAT::Input::IsKeyPressed(MAAT_KEY_DOWN))
 			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
+		//----- A, D ---------------------------------------
 		if (MAAT::Input::IsKeyPressed(MAAT_KEY_A))
 			m_CameraRotation += m_CameraRotationSpeed * ts;
 		if (MAAT::Input::IsKeyPressed(MAAT_KEY_D))
@@ -144,7 +150,18 @@ public:
 
 		MAAT::Renderer::BeginScene(m_Camera);
 
-		MAAT::Renderer::Submit(m_BlueShader, m_SquareVA);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				MAAT::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+		
 		MAAT::Renderer::Submit(m_Shader, m_VertexArray);
 
 		MAAT::Renderer::EndScene();
