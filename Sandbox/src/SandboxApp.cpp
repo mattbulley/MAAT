f998a691bbf8893efb2e71ce0b555c86d80e9686
mattbulley <mattbulley01@gmail.com>
2023-10-13 14:20:26 +0100

    Abstracted shader/uniforms and added colour picker.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <MAAT.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public MAAT::Layer
 {
@@ -88,7 +91,7 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new MAAT::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(MAAT::Shader::Create(vertexSrc, fragmentSrc));
 
 		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
@@ -112,17 +115,17 @@ public:
 
 			layout(location = 0) out vec4 color;
 
-			uniform vec4 u_Color;
-
 			in vec3 v_Position;
+
+			uniform vec3 u_Color;
 
 			void main()
 			{
-				color = u_Color;
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_FlatColorShader.reset(new MAAT::Shader(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
+		m_FlatColorShader.reset(MAAT::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(MAAT::Timestep ts) override
@@ -154,18 +157,15 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
-		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
-		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+		std::dynamic_pointer_cast<MAAT::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<MAAT::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				if (x % 2 == 0)
-					m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
-				else
-					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
 				MAAT::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
@@ -177,7 +177,9 @@ public:
 	
 	virtual void OnImGuiRender() override
 	{
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 	
 	void OnEvent(MAAT::Event& event) override
@@ -196,6 +198,8 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 100.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public MAAT::Application
