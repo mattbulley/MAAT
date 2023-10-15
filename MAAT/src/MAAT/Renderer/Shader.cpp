674fca27a6e76d093e83585f0b4b87b1a32cf653
mattbulley <mattbulley01@gmail.com>
2023-10-15 21:43:13 +0100

    Addded Shader Asset Files (shaders can be read from files).

--- MAAT/src/MAAT/Renderer/Shader.cpp
+++ MAAT/src/MAAT/Renderer/Shader.cpp
@@ -6,6 +6,18 @@
 
 namespace MAAT {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:    MAAT_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+		}
+
+		MAAT_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
