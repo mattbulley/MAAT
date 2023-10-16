d6748c4fbf8f369b89af1aab83003ef91860a84d
mattbulley <mattbulley01@gmail.com>
2023-10-16 22:04:51 +0100

    Added 2D Renderer Transforms.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -4,8 +4,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "Platform/OpenGL/OpenGLShader.h"
-
 Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f)
 {
@@ -13,12 +11,10 @@ Sandbox2D::Sandbox2D()
 
 void Sandbox2D::OnAttach()
 {
-	
 }
 
 void Sandbox2D::OnDetach()
 {
-
 }
 
 void Sandbox2D::OnUpdate(MAAT::Timestep ts)
@@ -31,13 +27,9 @@ void Sandbox2D::OnUpdate(MAAT::Timestep ts)
 	MAAT::RenderCommand::Clear();
 
 	MAAT::Renderer2D::BeginScene(m_CameraController.GetCamera());
-
-	MAAT::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	MAAT::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	MAAT::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 	MAAT::Renderer2D::EndScene();
-
-	// TODO: Add these functions - Shader::SetMat4, Shader::SetFloat4
-	// std::dynamic_pointer_cast<MAAT::OpenGLShader>(m_FlatColorShader)->Bind();
-	// std::dynamic_pointer_cast<MAAT::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 void Sandbox2D::OnImGuiRender()
