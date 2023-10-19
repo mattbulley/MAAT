0d48a33b08ad1ceffccd161cdf7d08ca6d323c9b
mattbulley <mattbulley01@gmail.com>
2023-10-19 17:07:55 +0100

    Updated DrawRotatedQuad to use batch rendering.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -36,13 +36,16 @@ void Sandbox2D::OnUpdate(MAAT::Timestep ts)
 	}
 
 	{
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
 		MAAT_PROFILE_SCOPE("Renderer Draw");
 		MAAT::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		//MAAT::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+		MAAT::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		MAAT::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		MAAT::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		MAAT::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f}, { 10.0f, 10.0f }, m_AlienTexture, 10.0f);
-		MAAT::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f}, { 1.0f, 1.0f }, m_AlienTexture, 20.0f);
+		MAAT::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f}, { 10.0f, 10.0f }, m_AlienTexture, 10.0f);
+		MAAT::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f}, { 1.0f, 1.0f }, rotation, m_AlienTexture, 20.0f);
 		MAAT::Renderer2D::EndScene();
 	}
 }
