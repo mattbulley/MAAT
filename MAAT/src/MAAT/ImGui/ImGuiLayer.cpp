b9cdd276b40bb631fab09ba71cefe07548f558ce
mattbulley <mattbulley01@gmail.com>
2023-10-26 00:24:02 +0100

    Updated colors

--- MAAT/src/MAAT/ImGui/ImGuiLayer.cpp
+++ MAAT/src/MAAT/ImGui/ImGuiLayer.cpp
@@ -7,6 +7,8 @@
 
 #include "MAAT/Core/Application.h"
 
+#include <glm/glm.hpp>
+
 // TEMPORARY
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
@@ -111,33 +113,49 @@ namespace MAAT {
 	void ImGuiLayer::SetDarkThemeColors()
 	{
 		auto& colors = ImGui::GetStyle().Colors;
-		colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+
+		ImVec4 navy = { 13.0f / 255.0f, 17.0f / 255.0f, 23.0f / 255.0f, 1.0f };
+		ImVec4 darkNavy = { 1.0f / 255.0f, 4.0f / 255.0f, 9.0f / 255.0f, 1.0f };
+		ImVec4 button = { 33.0f / 255.0f, 38.0f / 255.0f, 45.0f / 255.0f, 1.0f };
+		ImVec4 buttonLight = { 58.0f / 255.0f, 63.0f / 255.0f, 68.0f / 255.0f, 1.0f };
+		ImVec4 buttonLight2 = { 83.0f / 255.0f, 88.0f / 255.0f, 93.0f / 255.0f, 1.0f };
+		ImVec4 white = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+		//colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+		colors[ImGuiCol_WindowBg] = navy;
 
 		// Headers
-		colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		//colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_Header] = button;
+		//colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_HeaderHovered] = buttonLight;
+		colors[ImGuiCol_HeaderActive] = darkNavy;
 
 		// Buttons
-		colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		//colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_Button] = button;
+		colors[ImGuiCol_ButtonHovered] = buttonLight;
+		colors[ImGuiCol_ButtonActive] = darkNavy;
 
 		// Frame BG
-		colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		//colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_FrameBg] = button;
+		colors[ImGuiCol_FrameBgHovered] = buttonLight;
+		colors[ImGuiCol_FrameBgActive] = darkNavy;
 
 		// Tabs
-		colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
-		colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
-		colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_Tab] = darkNavy;
+		//colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
+		colors[ImGuiCol_TabHovered] = buttonLight2;
+		//colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
+		colors[ImGuiCol_TabActive] = buttonLight;
+		colors[ImGuiCol_TabUnfocused] = darkNavy;
+		colors[ImGuiCol_TabUnfocusedActive] = button;
 
 		// Title
-		colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		//colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBg] = darkNavy;
+		colors[ImGuiCol_TitleBgActive] = darkNavy;
+		colors[ImGuiCol_TitleBgCollapsed] = darkNavy;
 	}
 }
