dd038bab400bf5e50c1977b72d2c988f8a3c838b
mattbulley <mattbulley01@gmail.com>
2023-10-25 22:25:06 +0100

    Implemented adding/removing of entities and components.

--- MAAT/src/MAAT/Scene/Entity.h
+++ MAAT/src/MAAT/Scene/Entity.h
@@ -17,7 +17,9 @@ namespace MAAT {
 		T& AddComponent(Args&&... args)
 		{
 			MAAT_CORE_ASSERT(!HasComponent<T>(), "Entity already has component!");
-			return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			T& component = m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			m_Scene->OnComponentAdded<T>(*this, component);
+			return component;
 		}
 
 		template<typename T>
@@ -37,10 +39,11 @@ namespace MAAT {
 		void RemoveComponent()
 		{
 			MAAT_CORE_ASSERT(HasComponent<T>(), "Entity does not have component!");
-			m_Scene->m_Registry.remove<t>(m_EntityHandle);
+			m_Scene->m_Registry.remove<T>(m_EntityHandle);
 		}
 
 		operator bool() const { return m_EntityHandle != entt::null; }
+		operator entt::entity() const { return m_EntityHandle; }
 		operator uint32_t() const { return (uint32_t)m_EntityHandle; }
 
 		bool operator==(const Entity& other) const
