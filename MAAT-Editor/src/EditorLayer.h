d2bbd1e76764aec82fac29bb1cf154b72266861a
mattbulley <mattbulley01@gmail.com>
2023-11-05 23:41:00 +0000

    Added physics colliders visualization.

--- MAAT-Editor/src/EditorLayer.h
+++ MAAT-Editor/src/EditorLayer.h
@@ -24,6 +24,8 @@ namespace MAAT {
 		bool OnKeyPressed(KeyPressedEvent& e);
 		bool OnMouseButtonPressed(MouseButtonPressedEvent& e);
 
+		void OnOverlayRender();
+
 		void NewScene();
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
@@ -69,6 +71,8 @@ namespace MAAT {
 		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 
 		int m_GizmoType = 0;
+
+		bool m_ShowPhysicsColliders = false;
 
 		enum class SceneState
 		{
