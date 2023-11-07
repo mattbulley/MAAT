97b60b3397a723ec195157cd86e356f735d8eb6a
mattbulley <mattbulley01@gmail.com>
2023-11-07 11:51:55 +0000

    Simplified UUID hash specialization.

--- MAAT/src/MAAT/Core/UUID.h
+++ MAAT/src/MAAT/Core/UUID.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <xhash>
-
 namespace MAAT {
 
 	class UUID
@@ -19,13 +17,14 @@ namespace MAAT {
 }
 
 namespace std {
+	template <typename T> struct hash;
 
 	template<>
 	struct hash<MAAT::UUID>
 	{
 		std::size_t operator()(const MAAT::UUID& uuid) const
 		{
-			return hash<uint64_t>()((uint64_t)uuid);
+			return (uint64_t)uuid;
 		}
 	};
 }
