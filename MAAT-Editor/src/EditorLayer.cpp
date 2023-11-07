60318881e03e7f4f4be69bd612e9d7a4e4a58906
mattbulley <mattbulley01@gmail.com>
2023-11-07 10:44:17 +0000

    Update EditorLayer.cpp

--- MAAT-Editor/src/EditorLayer.cpp
+++ MAAT-Editor/src/EditorLayer.cpp
@@ -413,7 +413,10 @@ namespace MAAT {
 	void EditorLayer::OnEvent(Event& e)
 	{
 		m_CameraController.OnEvent(e);
-		m_EditorCamera.OnEvent(e);
+		if (m_SceneState == SceneState::Edit)
+		{
+			m_EditorCamera.OnEvent(e);
+		}
 
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<KeyPressedEvent>(MAAT_BIND_EVENT_FN(EditorLayer::OnKeyPressed));
